c25dd335605202826515e41191200968912ba3c0
llenotre <llenotre@student.42.fr>
2021-03-09 00:50:07 +0100

    Troubleshooting Q-Values updating

--- main.py
+++ main.py
@@ -17,24 +17,23 @@ def train(env):
 	layers.append(tf.keras.layers.Dense(2, activation='relu'))
 
 	model = tf.keras.Sequential(layers)
-	model.compile(optimizer='adam',
-		  loss=tf.keras.losses.MeanSquaredError(),
-		  metrics=['accuracy'])
+	model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
 	e = 0
 	while True:
 		env.reset()
+		env.render()
+		observation, _, _, _ = env.step(env.action_space.sample())
 
 		t = 0
 		while True:
-			env.render()
-
-			observation = env.observation_space
-			print(model.predict(np.transpose(observation.high)))
-			q_values = model.predict(np.transpose(observation.high))[0]
+			in_data = np.array([observation])
+			q_values = model.predict(in_data)[0]
 			max_q_value_id = get_max(q_values)
 			max_q_value = q_values[max_q_value_id]
-			_, reward, done, _ = env.step(max_q_value_id)
+
+			env.render()
+			next_observation, reward, done, _ = env.step(max_q_value_id)
 			if done:
 				reward = -1000
 			print('reward: ' + str(reward))
@@ -42,19 +41,21 @@ def train(env):
 			learning_rate = 0.5 # TODO Tune
 			discount_factor = 0.9 # TODO Tune
 			epochs_count = 10 # TODO Tune
+			print('old Q-Values: ' + str(q_values))
 			new_q_value = (1. - learning_rate) * max_q_value + learning_rate * (reward + discount_factor * max_q_value) # TODO fix
-			print('q_value: ' + str(learning_rate) + " " + str(max_q_value) + " " + str(reward) + " " + str(discount_factor) + " " + str(new_q_value))
 
 			new_q_values = q_values
 			new_q_values[max_q_value_id] = new_q_value
-			x = np.transpose([observation.high])
-			y = np.zeros((4, 1))
-			y[:2, :0] = np.transpose([new_q_values])
+			print('new Q-Values: ' + str(new_q_values))
+			x = np.array([observation])
+			y = np.array([new_q_values])
 			model.fit(x, y, epochs=epochs_count)
 
 			if done:
-				print('Episode', e, 'fails after', t, 'timesteps')
+				print('Episode' + str(e) + 'fails after' + str(t) + 'timesteps')
 				break;
+
+			observation = next_observation
 			t += 1;
 		e += 1
 
