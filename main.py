75077ca744e5794c019f295f83889b2cd1631d1a
llenotre <llenotre@student.42.fr>
2021-03-09 22:10:55 +0100

    Troubleshooting training

--- main.py
+++ main.py
@@ -9,63 +9,72 @@ def get_max(arr):
 			max_i = i
 	return max_i
 
-def train(env):
-	layers = []
-	layers.append(tf.keras.layers.Dense(4, activation='relu'))
-	for i in range(20):
-		layers.append(tf.keras.layers.Dense(20, activation='relu'))
-	layers.append(tf.keras.layers.Dense(2, activation='relu'))
+class GymModel:
+	def __init__(self):
+		self.env = gym.make('CartPole-v0')
 
-	model = tf.keras.Sequential(layers)
-	model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+	def __fini__(self):
+		self.env.close()
 
-	e = 0
-	while True:
-		env.reset()
-		env.render()
-		observation, _, _, _ = env.step(env.action_space.sample())
+	def train(self):
+		layers = []
+		layers.append(tf.keras.layers.Dense(4, activation='relu'))
+		for i in range(20):
+			layers.append(tf.keras.layers.Dense(20, activation='relu'))
+		layers.append(tf.keras.layers.Dense(2, activation='relu'))
 
-		t = 0
+		model = tf.keras.Sequential(layers)
+		model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+		e = 0
 		while True:
-			in_data = np.array([observation])
-			q_values = model.predict(in_data)[0]
-			print('in: ' + str(in_data))
-			print('out: ' + str(q_values))
-			max_q_value_id = get_max(q_values)
-			max_q_value = q_values[max_q_value_id]
+			self.env.reset()
+			self.env.render()
+			observation, _, _, _ = self.env.step(self.env.action_space.sample())
 
-			env.render()
-			next_observation, reward, done, _ = env.step(max_q_value_id)
-			if done:
-				reward = -100
-			else:
-				reward -= abs(next_observation[2])
-			print('reward: ' + str(reward))
+			t = 0
+			while True:
+				in_data = np.array([observation])
+				q_values = model.predict(in_data)[0]
+				print('bleh: ' + str(model.predict(in_data)))
+				print('in: ' + str(in_data))
+				print('out: ' + str(q_values))
+				max_q_value_id = get_max(q_values)
+				max_q_value = q_values[max_q_value_id]
 
-			learning_rate = 0.5 # TODO Tune
-			discount_factor = 0.5 # TODO Tune
-			epochs_count = 10 # TODO Tune
-			print('old Q-Values: ' + str(q_values))
-			new_q_value = (1. - learning_rate) * max_q_value + learning_rate * (reward + discount_factor * max_q_value) # TODO fix
+				self.env.render()
+				next_observation, reward, done, _ = self.env.step(max_q_value_id)
+				if done:
+					reward = -100
+				print('reward: ' + str(reward))
 
-			new_q_values = q_values
-			new_q_values[max_q_value_id] = new_q_value
-			print('new Q-Values: ' + str(new_q_values))
-			x = np.array([observation])
-			y = np.array([new_q_values])
-			print('x: ' + str(x))
-			print('y: ' + str(y))
-			model.fit(x, y, epochs=epochs_count)
+				in_data = np.array([observation])
+				next_q_values = model.predict(in_data)[0]
+				max_next_q_value = q_values[get_max(next_q_values)]
 
-			if done:
-				print('Episode ' + str(e) + ' fails after ' + str(t) + ' timesteps')
-				break;
+				learning_rate = 0.5 # TODO Tune
+				discount_factor = 0.5 # TODO Tune
+				epochs_count = 10 # TODO Tune
+				print('old Q-Values: ' + str(q_values))
+				new_q_value = (1. - learning_rate) * max_q_value + learning_rate * (reward + discount_factor * max_next_q_value)
 
-			prev_observation = observation
-			observation = next_observation
-			t += 1;
-		e += 1
+				new_q_values = q_values
+				new_q_values[max_q_value_id] = new_q_value
+				print('new Q-Values: ' + str(new_q_values))
+				x = np.array([observation])
+				y = np.array([new_q_values])
+				print('x: ' + str(x))
+				print('y: ' + str(y))
+				model.fit(x, y, epochs=epochs_count)
 
-env = gym.make('CartPole-v0')
-train(env)
-env.close()
+				if done:
+					print('Episode ' + str(e) + ' fails after ' + str(t) + ' timesteps')
+					break;
+
+				prev_observation = observation
+				observation = next_observation
+				t += 1;
+			e += 1
+
+model = GymModel()
+model.train()
